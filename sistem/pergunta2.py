84ab986d3b238c092b385749b1891c92c704bbba
JoaoGabriel-JG <joao.nitsus@gmail.com>
2023-03-20 10:18:00 -0300

    toArrumando

--- sistem/pergunta2.py
+++ sistem/pergunta2.py
@@ -2,21 +2,17 @@ print ('-' *30)
 print (' ' *3, 'Sequência de Fibonacci')
 print ('-' *30)
 
+fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181]
+
 valor = int(input('Insira um número: '))    # Um input com números inteiros pois a Fibonacci não recebe números float
-inicio1 = 0
-inicio2 = 1
 
-print ('{} → {}'.format(inicio1, inicio2), end='')    # Formatação da saída
-Contador = 3
+for i, n in enumerate(fibonacci):        # Estrutura de repetição 
+   print (' → {}'.format(n), end='')
 
-while Contador <= valor:        # Estrutura de repetição While 
-   valor1 = inicio1 + inicio2
-   print (' → {}'.format(valor1), end='')
-   inicio1 = inicio2
-   inicio2 = valor1
-   Contador += 1
-
-print (' → FIM')    
-
-# A sequencia em si já ira mostrar se o número pertence ou não
+   if valor == n:
+      print(' O valor está dentro da Fibonacci')
+      break
+   elif n == fibonacci[-1:]:
+      print('O valor não pertence a Fibonacci')
+  
 # Usei PY pois é minha segunda linguagem e nele da pra colocar inputs
